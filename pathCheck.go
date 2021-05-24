00054fe44c64997a8bab0923aeeb46937aacc29f
snailhuang <snailhuang@163.com>
2021-05-25 00:28:23 +0800

    fix struct error

--- pathCheck.go
+++ pathCheck.go
@@ -14,31 +14,30 @@ import (
 
 func checkPathDoubledAndExisted(cfg *Config) error {
 	doubledCheckMap := make(map[string]struct{})
-	for _,mp := range cfg.MiddleTmps {
-		err := checkPathExisted(mp.Path)
+	for _, mp := range cfg.MiddleTmps {
+		err := checkPathExisted(mp)
 		if err != nil {
 			return err
 		}
-		if _,ok := doubledCheckMap[mp.Path];!ok {
-			doubledCheckMap[mp.Path] = struct{}{}
-		}else {
-			return errors.New(fmt.Sprintf("%s doubled,please check config file first",mp.Path))
+		if _, ok := doubledCheckMap[mp]; !ok {
+			doubledCheckMap[mp] = struct{}{}
+		} else {
+			return errors.New(fmt.Sprintf("%s doubled,please check config file first", mp))
 		}
 	}
-	for _,fp := range cfg.FinalDirs {
-		err := checkPathExisted(fp.Path)
+	for _, fp := range cfg.FinalDirs {
+		err := checkPathExisted(fp)
 		if err != nil {
 			return err
 		}
-		if _,ok := doubledCheckMap[fp.Path];!ok {
-			doubledCheckMap[fp.Path] = struct{}{}
-		}else {
-			return errors.New(fmt.Sprintf("%s doubled,please check config file first",fp.Path))
+		if _, ok := doubledCheckMap[fp]; !ok {
+			doubledCheckMap[fp] = struct{}{}
+		} else {
+			return errors.New(fmt.Sprintf("%s doubled,please check config file first", fp))
 		}
 	}
 	return nil
 }
-
 
 func checkPathExisted(p string) error {
 	_, err := os.Stat(p)
